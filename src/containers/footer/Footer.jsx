ae2de454d4a07c2ac8242686d8f716289edbf316
lucianRosuAtc <lucian.rosu.atc@gmail.com>
2022-12-15 10:11:30 +0000

    update

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import gpt3Logo from'../../assets/logo.svg'
 import './footer.css'
 
 const Footer =()=> {
@@ -10,6 +11,15 @@ const Footer =()=> {
             
             <div className="gpt3__footer-btn"> 
                 <p>Request Early Access</p>
+            </div>
+            <div className="gpt3__footer-links">
+                <div className="gpt3__footer-links_logo">
+                    <img src={gpt3Logo} alt="logo" />
+                    <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+                </div>
+                <div className="gpt3__footer-links_div">
+                    <h4></h4>
+                </div>
             </div>
         </div>
     )
