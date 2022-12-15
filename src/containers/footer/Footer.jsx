238a4a89c3e254bddcf41ba77374d92b828c06f4
lucianRosuAtc <lucian.rosu.atc@gmail.com>
2022-12-15 11:17:46 +0000

    update

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -18,9 +18,28 @@ const Footer =()=> {
                     <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
                 </div>
                 <div className="gpt3__footer-links_div">
-                    <h4></h4>
+                    <h4>Links</h4>
+                    <p>Overons</p>
+                    <p>Social Media</p>
+                    <p>Counters</p>
+                    <p>Contact</p>
+                </div>
+                <div className="gpt3__footer-links_div">
+                    <h4>Company</h4>
+                    <p>Terms & Conditions</p>
+                    <p>Privacy Policy</p>
+                    <p>Contact</p>
+                </div>
+                <div className="gpt3__footer-links_div">
+                    <h4>Get in touch</h4>
+                    <p>Crechterwoord K12 182 DK Alknjkcb</p>
+                    <p>085-132567</p>
+                    <p>info@payme.net</p>
                 </div>
             </div>
+                <div className="gpt3__footer-copyright">
+                    <p>© 2021 GPT-3. All rights reserved.</p>
+                </div>
         </div>
     )
 }
